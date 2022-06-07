2a164e49bf2caba8122158f0e17135f9c2030c9f
LuYutt <93062752+LuYutt@users.noreply.github.com>
2022-06-07 17:55:20 +0800

    Add files via upload

--- SQL_option.py
+++ SQL_option.py
@@ -3,13 +3,16 @@
 作者:Yutang Lu
 日期:2022年06月04日
 """
-from SpiderDate import new_total_datalist
+
+#from SpiderDate import new_total_datalist
 from flask import Flask
 from sqlalchemy import and_, or_, desc, func
 from init import db
 from models import Menu
 from flask_sqlalchemy import SQLAlchemy
-add_flag = 1
+import json
+
+add_flag = 0
 delete_flag = 0
 delete_date = '2022-06-03'  # 输入要删除的日期
 menus = []
@@ -19,7 +22,6 @@ if count == 0:
 else:
     max_id = db.session.query(func.max(Menu.id)).first()[0]
     menu_id = max_id + 1
-
 
 if add_flag:
     for i in new_total_datalist:
@@ -35,5 +37,22 @@ if delete_flag:
     for i in temp:
         db.session.delete(i)
         db.session.commit()
+
+# 暂时的操作
+
+
+with open('Menus.json', 'w', encoding='utf-8') as file:
+    date = Menu.query.filter(Menu.id > 0).all()
+    menu_dic = []
+    for i in date:
+        menu = {'id': i.id, '菜名': i.menu, '餐厅': i.canteen, '日期': str(i.date), '时段': i.time}
+        menu_dic.append(menu)
+    json.dump(menu_dic, file, indent='\n')
+
+with open('Menus.txt', 'w', encoding='utf-8') as file:
+    date = Menu.query.filter(Menu.id > 0).all()
+    for i in date:
+        menu = 'id:'+str(i.id) + '\t菜名:'+i.menu+'\t餐厅:'+i.canteen+'\t日期:'+str(i.date)+'\t时段:'+i.time+'\n'
+        file.write(menu)
 
 
--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 作者:Yutang Lu
 日期:2022年06月04日
 """
-from flask import render_template
-
+from flask import render_template, request
+from sqlalchemy.orm import context
+from init import db
 from init import app
+from models import Menu
 
 
 @app.route('/')
@@ -13,26 +15,24 @@ def index():
     # UserAgent = request.headers.get('user-agent')
     # return f'hello!{UserAgent}'
     # 类
-    class Person(object):
-        name = u'luyutang'
-        age = 20
-    I = Person()
-    context = {
-        'username': u'luyutang',
-        'gender': u'男',
-        'age': 22,
-        'person': I,  # 声明
-        'websites': {
-            'baidu': 'www.baidu.com',
-            'google': 'www.google.com'
-        }
-    }
-    return render_template('index.html', **context)
+    temp = db.session.query(Menu).filter(Menu.id>=1).all()
+    canteen = []
+    date = []
+    for dish in temp:
+        if dish.canteen not in canteen:
+            canteen.append(dish.canteen)
+        if dish.date not in date:
+            date.append(dish.date)
+    return render_template('index.html', canteen=canteen, date=date)
 
 
-@app.route('/user/<name>')
-def hello(name):
-    return f'hello {name}!!'
+@app.route('/choose', methods=['GET', 'POST'])
+def choose():
+    if request.method == 'POST':
+        canteen = request.form.get("canteen")
+        date = request.form.get("date")
+        menus = db.session.query(Menu).filter(Menu.date==date, Menu.canteen==canteen).all()
+    return render_template('ChooseShow.html', menus=menus)
 
 
 if __name__ == '__main__':
